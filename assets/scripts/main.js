0e9635e4c445470e33d1dc523d473d238b995cdf
Leman <leman@Leman-MacBook-Air.local>
2023-05-16 12:31:39 +0400

    created2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,13 +5,13 @@ let all = document.querySelectorAll(".wrap");
 
 tabs.forEach((tab)=>{
 	tab.addEventListener("click", ()=>{
-		localStorage.setItem('selectedTab', tab.getAttribute("data-target"));
+		localStorage.setItem('selectedTab', tab.getAttribute("data-tab"));
 		tabs.forEach((tab)=>{
 			tab.classList.remove("active");
 		})
 
 		tab.classList.add("active");
-		let tabName = tab.getAttribute("data-target");
+		let tabName = tab.getAttribute("data-tab");
 		all.forEach((item)=>{
 			item.style.display = "none";
 		})
@@ -30,19 +30,36 @@ tabs.forEach((tab)=>{
 				item.style.display = "block";
 			})
 		}
-		document.getElementById(this.getAttribute("data-target")).style.display = 'block';
+		document.getElementById(this.getAttribute("data-tartabget")).style.display = 'block';
   });
   
 
 });
 const storedTab = localStorage.getItem('selectedTab');
-		if (storedTab) {
-			const selectedTab = document.querySelector(`[data-tab="${storedTab}"]`);
-			if (selectedTab) {
-			  selectedTab.classList.add('active');
-			  document.getElementById(storedTab).style.display = 'block';
-			}
-		  }else {
-			tabs[0].classList.add('active');
-			tabContents[0].style.display = 'block';
-		  }
+if (storedTab) {
+  const selectedTab = document.querySelector(`[data-tab="${storedTab}"]`);
+  if (selectedTab) {
+    selectedTab.classList.add('active');
+    all.forEach((item) => {
+      item.style.display = "none";
+    });
+    if (storedTab == "male") {
+      males.forEach((male) => {
+        male.style.display = "block";
+      });
+    } else if (storedTab == "female") {
+      females.forEach((female) => {
+        female.style.display = "block";
+      });
+    } else {
+      all.forEach((item) => {
+        item.style.display = "block";
+      });
+    }
+  }
+} else {
+  tabs[0].classList.add('active');
+  all.forEach((item) => {
+    item.style.display = "block";
+  });
+}
